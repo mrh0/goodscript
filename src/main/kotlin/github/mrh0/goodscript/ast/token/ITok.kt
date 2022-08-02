27f1f1289a1ab7353f0d52bed18ded69f3679967
MRH0 <mrhmail1@gmail.com>
2022-08-02 23:45:09 +0200

    20220802b

--- src/main/kotlin/github/mrh0/goodscript/ast/token/ITok.kt
+++ src/main/kotlin/github/mrh0/goodscript/ast/token/ITok.kt
@@ -1,7 +1,10 @@
 package github.mrh0.goodscript.ast.token
 
+import github.mrh0.goodscript.ir.IR
+import github.mrh0.goodscript.types.GsType
+import github.mrh0.goodscript.vm.VM
 import org.antlr.v4.runtime.Token
 
 interface ITok {
-
+    fun process(vm: VM, list: MutableList<IR>): GsType
 }
--- src/main/kotlin/github/mrh0/goodscript/ast/token/TInteger.kt
+++ src/main/kotlin/github/mrh0/goodscript/ast/token/TInteger.kt
@@ -1,11 +1,21 @@
 package github.mrh0.goodscript.ast.token
 
 import github.mrh0.goodscript.ast.Loc
+import github.mrh0.goodscript.ir.IR
+import github.mrh0.goodscript.ir.IRInteger
+import github.mrh0.goodscript.types.GsInteger
+import github.mrh0.goodscript.types.GsType
+import github.mrh0.goodscript.vm.VM
 import org.antlr.v4.runtime.Token
 import java.io.File
 
-class TInteger(val value: Int, location: Loc) : Tok(location) {
+class TInteger(val value: Int, location: Loc) : Tok(location) /*IR?*/ {
     override fun toString(): String {
         return "${value}i"
+    }
+
+    override fun process(vm: VM, list: MutableList<IR>): GsType {
+        list.add(IRInteger(value, location))
+        return GsInteger(value)
     }
 }
