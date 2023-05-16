8bf42539d5f2edb7cd692fb330e798ed27260e32
MRH0 <mrhmail1@gmail.com>
2023-05-17 00:55:19 +0200

    Create TLambda.kt

---
+++ src/main/kotlin/github/mrh0/goodscript/ast/token/function/TLambda.kt
@@ -0,0 +1,36 @@
+package github.mrh0.goodscript.ast.token.function
+
+import github.mrh0.goodscript.ast.CompileData
+import github.mrh0.goodscript.ast.ITok
+import github.mrh0.goodscript.ast.Loc
+import github.mrh0.goodscript.ast.Tok
+import github.mrh0.goodscript.ast.token.TBlock
+import github.mrh0.goodscript.ir.IIR
+import github.mrh0.goodscript.ir.IRBlock
+import github.mrh0.goodscript.ir.function.IRFunc
+import github.mrh0.goodscript.types.GsTypeBase
+import github.mrh0.goodscript.types.GsTypeNone
+import github.mrh0.goodscript.values.GsValueNone
+import github.mrh0.goodscript.vm.state.Variable
+
+class TLambda(location: Loc, val expr: ITok, val args: MutableList<TArgument>, val returns: ITok) : Tok(location) {
+    override fun toString() = "TLambda($args, $expr)"
+
+    private var funcIR: IRFunc? = null
+    fun getFuncIR() = funcIR ?: throw Exception("User function supplier is empty. This should never happen!")
+    fun processSignature(cd: CompileData): Pair<List<Pair<String, GsTypeBase>>, GsTypeBase> {
+        val argPairs = args.map { Pair(it.name, it.process(cd).first) }
+        val returnType = returns.process(cd).first
+        return Pair(argPairs, returnType)
+    }
+
+    override fun process(cd: CompileData): Pair<GsTypeBase, IIR> {
+        //cd.newContext(name)
+        val argPairs = args.map { Pair(it.name, it.process(cd).first) }.toTypedArray()
+        argPairs.forEach { cd.ctx().define(location, Variable(it.first, it.second, GsValueNone)) }
+        val returnType = returns.process(cd).first
+        val ir = expr.process(cd)
+        funcIR = IRFunc(location, ir.second as IRBlock, "name", argPairs, returnType)
+        return Pair(GsTypeNone, funcIR!!)
+    }
+}
